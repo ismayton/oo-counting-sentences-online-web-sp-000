67fa9955d4f5ef7be29e992dc09e5fd587b9d499
Ian Mayton <ian.mayton@gmail.com>
2020-07-21 20:50:59 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    counter = 0 
+    if self.sentence?
+      counter += 1 
+      elsif self.question?
+      counter += 1 
+      elsif self.exclamation? 
+      counter += 1 
+    end
   end
+  
 end
